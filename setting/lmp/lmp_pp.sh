bdeaed69957bac4b7a3ca79dd1543cbdcdf00988
lidanqing-intel <danqing.li@intel.com>
2022-06-03 13:15:51 +0000

    update

--- setting/lmp/lmp_pp.sh
+++ setting/lmp/lmp_pp.sh
@@ -4,9 +4,9 @@
 #echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo
 #echo 0 > /proc/sys/kernel/numa_balancing
 #cpupower frequency-set -g performance
-#export KMP_SETTINGS=1
-#export KMP_BLOCKTIME=0
-#export KMP_AFFINITY=granularity=fine,verbose,compact,1,0
+export KMP_SETTINGS=1
+export KMP_BLOCKTIME=0
+export KMP_AFFINITY=granularity=fine,verbose,compact,1,0
 #export TF_NUM_INTRAOP_THREADS=32
 #export TF_INTRA_OP_PARALLELISM_THREADS=64
 #export TF_INTRA_OP_PARALLELISM_THREADS=1
@@ -18,10 +18,16 @@ OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THRE
 # lmp_serial 8 threads
 OMP_NUM_THREADS=8 TF_INTRA_OP_PARALLELISM_THREADS=8 TF_INTER_OP_PARALLELISM_THREADS=1 /usr/bin/time -v numactl -c 0 -m 0 lmp_serial -in in.lammps
 
-# mpi more than 10 threads will crash
-# mpi  4 threads
-OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THREADS=1  mpirun --allow-run-as-root -np 4 lmp_mpi -in in.lammps
+# lmp_serial 8 threads
+#OMP_NUM_THREADS=12 TF_INTRA_OP_PARALLELISM_THREADS=12 TF_INTER_OP_PARALLELISM_THREADS=1 /usr/bin/time -v numactl -c 0 -m 0 lmp_serial -in in.lammps
 
-# mpi 8 threads
-OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THREADS=1  mpirun --allow-run-as-root -np 8 lmp_mpi -in in.lammps
-
+## mpi more than 10 threads will crash
+## mpi  4 threads
+#OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THREADS=1  mpirun --allow-run-as-root -np 4 lmp_mpi -in in.lammps
+#
+## mpi 8 threads
+#OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THREADS=1  mpirun --allow-run-as-root -np 8 lmp_mpi -in in.lammps
+#
+## mpi 12 threads
+#OMP_NUM_THREADS=1 TF_INTRA_OP_PARALLELISM_THREADS=1 TF_INTER_OP_PARALLELISM_THREADS=1  mpirun --allow-run-as-root -np 12 lmp_mpi -in in.lammps
+#
